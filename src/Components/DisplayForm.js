cdc6e65bc8b03ef489ec44d7c10ac484e93c76d3
aryanDhakad <67039949+aryanDhakad@users.noreply.github.com>
2022-09-28 00:58:49 +0530

    Updated Form builder

--- src/Components/DisplayForm.js
+++ src/Components/DisplayForm.js
@@ -1,26 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useForm } from "../Context/FormContext";
+import DisplayFormComp from "./DisplayFormComp";
 
-const DisplayForm = ({ data }) => {
+function DisplayForm() {
+  const { list } = useForm();
+
   return (
     <div>
-        <h3>Form : {data.metadata.formId} </h3>
-      <h3>Created By : {data.metadata.creatorName} </h3>
-      <h3>Created At : {data.metadata.createTime} </h3>
-      <pre> {JSON.stringify(data, null, 2)} </pre>
-      <form>
-        {Object.keys(data.data).map((item, index) => {
-          return (
-            <div key={index}>
-              <h3>
-                {data.data[item].title} : {data.data[item].name}
-              </h3>
-              <br />
-            </div>
-          );
-        })}
-      </form>
+      {/* {list.map((item) => (
+        <>{JSON.stringify(item, null, 2)}</>
+      ))} */}
+      {list.map((item, index) => (
+        <DisplayFormComp key={index} data={item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default DisplayForm
+export default DisplayForm;
--- src/Components/Mainbody.js
+++ src/Components/Mainbody.js
@@ -1,22 +1,59 @@
-import React from 'react'
-import form from './form.png';
-import './Mainbody.css'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import form from "./form.png";
+import "./Mainbody.css";
 
 const Mainbody = () => {
+  const navigate = useNavigate();
+  const goToCreateForm = () => {
+    navigate("/createForm");
+  };
+  const goToViewForm = () => {
+    navigate("/showForm");
+  };
   return (
-    <div className='main_body'>
+    <>
+      {" "}
+      <div className="main_body">
         <div className="mainbody_top">
-        <h3 style={{fontSize:"16px" , fontWeight:'500'}}>Created/Saved Forms</h3>
+          <h3 style={{ fontSize: "16px", fontWeight: "500" }}>
+            Created/Saved Forms
+          </h3>
         </div>
         <div className="mainbody_docs">
-           <div className='doc_card'>
-                <img src={form} alt="form" height="100px"  />
-                <p className="creator_name" style={{textAlign:"center"}}>Form Id </p>
-            </div> 
+          <div className="doc_card">
+            <img
+              onClick={() => goToCreateForm()}
+              src={form}
+              alt="form"
+              height="100px"
+            />
+            <p className="creator_name" style={{ textAlign: "center" }}>
+              Form Id{" "}
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
+      <div className="main_body">
+        <div className="mainbody_top">
+          <h3 style={{ fontSize: "16px", fontWeight: "500" }}>View Forms</h3>
+        </div>
+        <div className="mainbody_docs">
+          <div className="doc_card">
+            <img
+              onClick={() => goToViewForm()}
+              src={form}
+              alt="form"
+              height="100px"
+            />
+            <p className="creator_name" style={{ textAlign: "center" }}>
+              Form Id{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Mainbody
+export default Mainbody;
